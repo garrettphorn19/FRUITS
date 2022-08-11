0dcd533daa9dcb8c6061482a1b622f22bf9be015
Garrett Horn <garrett@sbfmail.com>
2022-08-11 16:32:14 -0400

    Initial Commit

---
+++ FRUITS/FRUITSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FRUITSApp.swift
+//  FRUITS
+//
+//  Created by Garrett Horn on 8/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FRUITSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
